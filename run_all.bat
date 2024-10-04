7b30ba8cd887325b8856b2368afc17aadec50ab5
abdurrehman0206 <abdurrehmansaleem5@gmail.com>
2024-10-04 17:34:32 +0500

    Init

--- run_all.bat
+++ run_all.bat
@@ -1,19 +1,24 @@
 @echo off
 
 echo Activating virtual environment...
-cd E:\Coding\Pablo\Chat with SQL\.venv\Scripts
+cd Z:\ReactJS\ChatDB\.venv\Scripts
 call activate
 
-echo Virtual environment activated. Starting the Python model...
-cd E:\Coding\Pablo\Chat with SQL
+echo Virtual environment activated.
 
-rem Start the Python model in a new command window and return control to the batch file
+echo Installing necessary Python packages...
+cd Z:\ReactJS\ChatDB
+pip install -r requirements.txt
+
+echo Necessary Python packages installed.
+
+echo Starting the Python model...
 start cmd /k python text_to_sql.py
 
 echo Python model started.
 
 echo Now starting Node.js backend...
-cd E:\Coding\Pablo\Chat with SQL\sql-chat
+cd Z:\ReactJS\ChatDB\WebInterface
 
 rem Start the Node.js server in a new command window and return control to the batch file
 start cmd /k node server.js
@@ -21,10 +26,8 @@ start cmd /k node server.js
 echo Node.js backend started.
 
 echo Now starting React frontend...
-cd E:\Coding\Pablo\Chat with SQL\sql-chat
+cd Z:\ReactJS\ChatDB\WebInterface
 npm start
 
 echo All processes are running. Press Ctrl+C to stop.
 pause
-
-
